d526958579c6cc24d27c1019528128d34f8cbb01
동현 신 <sambart17@gmail.com>
2020-10-15 01:32:01 +0900

    16922 complete

--- 16922.py
+++ 16922.py
@@ -1,16 +1,25 @@
 import sys
-numLIst = [1, 5, 10, 20]
+rst = 0
+numLIst = [1, 5, 10, 50]
+rstList = [0 for i in range(2000)]
+str_in = int(sys.stdin.readline())
 
-str_in = sys.stdin.readline()
 
-
-def solve(value, deepCnt):
-    if int(str_in) == deepCnt:
-        print(value)
+def solve(value, nVal, deepCnt):
+    global rst
+    if str_in == deepCnt:
+        # print(value)
+        if rstList[value] == 0:
+            rstList[value] = 1
+            rst = rst + 1
+            #rstList += 1
         return
-    for var in numLIst:
-        solve(value + var, deepCnt + 1)
-    return
+    for i in range(nVal, 4):
+        # print(i)
+        solve(value + numLIst[i], i, deepCnt + 1)
+    #return
 
-solve(0, 0)
-print(numLIst)
+
+solve(0, 0, 0)
+
+print(rst)
